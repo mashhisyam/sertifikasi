2e5085aee36e35020e5e329e78414c2404f315fb
mashhisyam <haryoh48@gmail.com>
2023-09-28 11:21:23 +0700

    initial

---
+++ lib/svg.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const iconIncome = 'assets/income.svg';
+const iconExpense = 'assets/Expense.svg';
+const iconSetting = 'assets/Setting.svg';
+const iconNotes = 'assets/Notes.svg';
